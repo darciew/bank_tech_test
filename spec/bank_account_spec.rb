5bc2cb8aaad6d379eac03263347ac737200d576c
Darcie <darciewalsh@hotmail.com>
2018-11-05 11:48:32 +0000

    Add BankAccount class with a starting balance of zero

---
+++ spec/bank_account_spec.rb
@@ -0,0 +1,13 @@
+require 'bank_account'
+
+describe BankAccount do
+  let(:bankaccount) { described_class.new }
+
+  context('A new bank account') do
+    describe('#initialize') do
+      it('starts with a balance of zero') do
+        expect(bankaccount.balance).to eq 0
+      end
+    end
+  end
+end
