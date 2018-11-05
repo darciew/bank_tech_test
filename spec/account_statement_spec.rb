bc08d9716ef5835f24746af74fc2a1f5c27a35d9
Darcie <darciewalsh@hotmail.com>
2018-11-05 16:35:41 +0000

    transaction_log logs withdraw transaction

--- spec/account_statement_spec.rb
+++ spec/account_statement_spec.rb
@@ -3,6 +3,7 @@ require 'account_statement'
 describe AccountStatement do
   let(:accountstatement) { described_class.new }
   let(:deposit_trans) { { credit: 0, debit: 150, balance: 150 } }
+  let(:withdraw_trans) { { credit: 150, debit: 0, balance: 0 } }
 
   context('A new account statement') do
     describe('#initialize') do
@@ -16,7 +17,12 @@ describe AccountStatement do
     describe('#transaction_log') do
       it('logs a deposit transaction') do
         accountstatement.transaction_log(deposit_trans)
-        expect(accountstatement.transactions).to eq [deposit_trans]
+        expect(accountstatement.transactions).to include { deposit_trans }
+      end
+
+      it('logs a withdraw transaction') do
+        accountstatement.transaction_log(withdraw_trans)
+        expect(accountstatement.transactions).to include { withdraw_trans }
       end
     end
   end
