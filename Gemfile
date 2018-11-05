c607c847ce13aac01eaef19d2c9eb0fa39e22d0d
Darcie <darciewalsh@hotmail.com>
2018-11-05 11:21:43 +0000

    Project setup up with RSpec and Simplecov RSpec used for testing suite and Simplecov used for tracking test coverage

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'https://rubygems.org'
+
+group :test do
+  gem 'pry'
+  gem 'rspec'
+  gem 'simplecov'
+  gem 'simplecov-console'
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,9 @@
+require 'pry'
+require 'simplecov'
+require 'simplecov-console'
+
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::Console,
+])
+SimpleCov.start
