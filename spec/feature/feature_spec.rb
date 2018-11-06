85af726c4708ac708f814d93567fe9470445255e
Darcie <darciewalsh@hotmail.com>
2018-11-06 15:01:56 +0000

    Add feature test

---
+++ spec/feature/feature_spec.rb
@@ -0,0 +1,39 @@
+require 'bank_account'
+require 'account_statement'
+
+describe 'Features' do
+  context('Transactions') do
+    describe('Deposit') do
+      it('can deposit money in User\'s account') do
+        account = BankAccount.new
+        account.deposit(50)
+
+        expect(account.balance).to eq 50
+      end
+    end
+
+    describe('Withdraw') do
+      it('can withdraw money from User\'s account') do
+        account = BankAccount.new
+        account.deposit(150)
+        account.withdraw(50)
+
+        expect(account.balance).to eq 100
+      end
+    end
+  end
+
+  context('Statement') do
+    describe('View Statement') do
+      it('can display the User\'s account statement') do
+        account = BankAccount.new
+        account.deposit(150)
+        account.withdraw(50)
+
+        expect { account.view_statement }.to output('date || credit || '\
+           "debit || balance\n06/11/2018 || 150 || - || 150\n06/11/2018"\
+           " || - || 50 || 100\n").to_stdout
+      end
+    end
+  end
+end
